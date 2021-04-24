e283e03188dc0b49cb45b06e352a15ae116e931e
佐藤　弘樹 <re22sugar@gmail.com>
2021-04-24 18:51:45 +0900

    発注状況完了

--- app/Order.php
+++ app/Order.php
@@ -23,4 +23,9 @@ class Order extends Model
     return self::where('id', $id)->update($update); //第一引数はカラム、第二は登録ボタン押したときに登録されるレコードのid
     // 記述方法：Sample::where('検索値')->update([更新する値]);
   }
+
+  public static function changeStatus($change, $id)
+  {
+    self::where('id', $id)->update($change);
+  }
 }
